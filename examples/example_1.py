f07977f8e576f4f448d7102ce6ff545caa23ac2a
Ahmet Burhan Kayalı <ahmetburhan1703@gmail.com>
2025-02-20 22:18:39 +0300

    Update example_1.py

--- examples/example_1.py
+++ examples/example_1.py
@@ -1,8 +1,9 @@
 from simple_yt_api import YouTubeAPI
 
 
-youtube_link = "https://www.youtube.com/watch?v=BryspbM6s3E&list=PLQJij7idQLKxB0uXzHfiAgFP4OIPBiH5e"
-data, transcript = YouTubeAPI(youtube_link).get_video_data_and_transcript()
+url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+yt = YouTubeAPI(url)
 
+data, transcript = yt.get_video_data_and_transcript()
 print("Data:", data)
 print("Video transcript:", transcript)
