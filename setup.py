2bd10dfe4677a969df8effea61503203b69ae8c6
Ahmet Burhan Kayalı <ahmetburhan1703@gmail.com>
2025-02-23 20:03:21 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="simple-yt-api",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[
-        "requests",
-        "beautifulsoup4",
-        "youtube-transcript-api"
+        "requests>=2.32.3",
+        "beautifulsoup4>=4.13.3",
+        "youtube-transcript-api>=0.6.3"
     ],
     author="Ahmet Burhan Kayalı",
     author_email="ahmetburhan1703@gmail.com",
