bd77a413ac256895e528f30b0dbb82758ea0d5a2
Ahmet Burhan Kayalı <ahmetburhan1703@gmail.com>
2025-04-13 16:52:56 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="simple-yt-api",
-    version="1.1.0",
+    version="1.1.1",
     packages=find_packages(),
     install_requires=[
         "requests>=2.32.3",
