73fbb0c455910d954bae1d3a7750bde51b217064
SoAp9035 <ahmetburhan1703@gmail.com>
2025-06-25 19:57:36 +0300

    delete url validator function

--- simple_yt_api/utils.py
+++ simple_yt_api/utils.py
@@ -1,24 +1,3 @@
-def is_valid_youtube_url(url: str) -> bool:
-    """
-    Check if the given URL is a valid YouTube video.
-
-    Args:
-        url (str): The URL to validate.
-
-    Returns:
-        bool: True if the URL is a valid YouTube link, False otherwise.
-    """
-    if url.startswith((
-        "https://youtu.be/",
-        "https://youtube.com/watch?v=",
-        "https://www.youtube.com/watch?v=",
-        "https://youtube.com/shorts/",
-        "https://www.youtube.com/shorts/"
-        )):
-        return True
-    else:
-        return False
-
 def transcript_list_to_text(transcript_dict_list: list[dict]) -> str:
     """
     Convert a list of transcript dictionaries to a single text string.
@@ -32,4 +11,4 @@ def transcript_list_to_text(transcript_dict_list: list[dict]) -> str:
     transcript_text = ""
     for tct in transcript_dict_list:
         transcript_text += " " + tct["text"]
-    return transcript_text.replace("  ", " ")
+    return transcript_text.replace("  ", " ").strip()
