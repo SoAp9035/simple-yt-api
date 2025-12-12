9079f0151d903f6dd7391c9f3e5625df58c35014
SoAp9035 <ahmetburhan1703@gmail.com>
2025-12-12 15:22:30 +0300

    Format code

--- src/simple_yt_api/main.py
+++ src/simple_yt_api/main.py
@@ -1,5 +1,3 @@
-import time
-import random
 import logging
 import requests
 from bs4 import BeautifulSoup
@@ -11,7 +9,7 @@ from .exceptions import (
     NoVideoFound,
     NoMetadataFound,
     TranscriptsDisabled,
-    NoTranscriptFound
+    NoTranscriptFound,
 )
 
 
@@ -19,10 +17,9 @@ class YouTubeAPI:
     """
     A simple API to fetch YouTube video metadata and transcripts.
     """
+
     def __init__(self) -> None:
-        self._user_agent = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-        }
+        self._user_agent = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 
     def data(self, url: str) -> dict:
         """
@@ -31,10 +28,10 @@ class YouTubeAPI:
             - `title`: Video title
             - `img_url`: Thumbnail URL
             - `short_description`: Short video description
-        
+
         Args:
             url (str): The URL of the YouTube video.
-        
+
         Returns:
             dict: Video metadata
 
@@ -45,48 +42,48 @@ class YouTubeAPI:
         response = requests.get(url, headers=self._user_agent, timeout=10)
         if response.status_code != 200:
             raise NoVideoFound
-        
+
         youtube_html = response.text
         soup = BeautifulSoup(youtube_html, "html.parser")
         try:
             video_id = soup.find(name="meta", property="og:url").get("content")[32:]
             title = soup.find(name="meta", property="og:title").get("content")
             img_url = soup.find(name="meta", property="og:image").get("content")
-            description = soup.find(name="meta", property="og:description").get("content")
+            description = soup.find(name="meta", property="og:description").get(
+                "content"
+            )
         except Exception:
             raise NoMetadataFound
 
-        data = {
+        return {
             "video_id": video_id,
             "title": title,
             "img_url": img_url,
-            "short_description": description
+            "short_description": description,
         }
 
-        return data
-
-    def get_transcript(self, url: str, language_code: str = "en", as_dict: bool = True) -> list[dict] | str:
+    def get_transcript(
+        self, url: str, language_code: str = "en", as_dict: bool = True
+    ) -> list[dict] | str:
         """
         Returns the transcript of the video in requested language.
-        
+
         Args:
             url (str): The URL of the YouTube video.
             language_code (str, optional): The language code for the desired transcript. Defaults to "en".
             as_dict (bool, optional): If `True`, returns the transcript as a list of dictionaries;
                 otherwise, returns the transcript as a string. Defaults to `True`.
-        
+
         Returns:
             list[dict] | str: The transcript in the requested format (list of dictionaries or string).
-        
+
         Raises:
             YouTubeAPIError: Youtube API Error
             TranscriptsDisabled: Transcripts Disabled
             NoTranscriptFound: No Transcript Found
-        """        
+        """
         try:
-            time.sleep(random.uniform(0.25, 0.75))
             data = self.data(url)
-            time.sleep(random.uniform(0.25, 0.75))
 
             video_id = data["video_id"]
 
@@ -98,12 +95,14 @@ class YouTubeAPI:
             raise TranscriptsDisabled
         except _errors.NoTranscriptFound:
             try:
-                language_codes = [transcript.language_code for transcript in transcript_list]
+                language_codes = [
+                    transcript.language_code for transcript in transcript_list
+                ]
                 if "en" in language_codes:
                     transcript = transcript_list.find_transcript(["en"])
                 else:
                     transcript = transcript_list.find_transcript([language_codes[0]])
-                
+
                 translated_transcript = transcript.translate(language_code)
                 transcript_dict_list = translated_transcript.fetch().to_raw_data()
             except Exception:
@@ -111,12 +110,18 @@ class YouTubeAPI:
         except Exception:
             raise YouTubeAPIError
 
-        return transcript_dict_list if as_dict else transcript_list_to_text(transcript_dict_list)
-        
-    def get_video_data_and_transcript(self, url: str, language_code: str = "en", as_dict: bool = True) -> tuple:
+        return (
+            transcript_dict_list
+            if as_dict
+            else transcript_list_to_text(transcript_dict_list)
+        )
+
+    def get_video_data_and_transcript(
+        self, url: str, language_code: str = "en", as_dict: bool = True
+    ) -> tuple:
         """
         Returns both video metadata and transcript. If there is an error, that spot in the tuple will have `None` instead of a value.
-        
+
         Args:
             url (str): The URL of the YouTube video.
             language_code (str, optional): The language code for the desired transcript. Defaults to "en".
@@ -128,10 +133,10 @@ class YouTubeAPI:
                 - transcript (list[dict] | str): Video transcript, `None` if not found
         """
         try:
-            time.sleep(random.uniform(0.25, 0.75))
             data = self.data(url)
-            time.sleep(random.uniform(0.25, 0.75))
-            transcript = self.get_transcript(url=url, language_code=language_code, as_dict=as_dict)
+            transcript = self.get_transcript(
+                url=url, language_code=language_code, as_dict=as_dict
+            )
         except (TranscriptsDisabled, NoTranscriptFound) as e:
             transcript = None
             logging.warning(f"Simple YT API: {e}")
