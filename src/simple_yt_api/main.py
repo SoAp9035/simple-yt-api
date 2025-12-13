9661858049b0e79b2c0d9936625799d6f6191030
SoAp9035 <ahmetburhan1703@gmail.com>
2025-12-13 15:59:28 +0300

    Add _extract_video_id method

--- src/simple_yt_api/main.py
+++ src/simple_yt_api/main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from .models import VideoMetadata
 from .utils import transcript_list_to_text
 from youtube_transcript_api import _errors
+from urllib.parse import urlparse, parse_qs
 from youtube_transcript_api import YouTubeTranscriptApi
 from .exceptions import (
     YouTubeAPIError,
@@ -22,7 +23,33 @@ class YouTubeAPI:
     def __init__(self) -> None:
         self._user_agent = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 
-    def fetch_metadata(self, url: str) -> VideoMetadata:
+    def _extract_video_id(self, url_or_id: str) -> str:
+        """Returns video id."""
+        if not any(x in url_or_id for x in ["://", "youtube.com", "youtu.be"]):
+            return url_or_id
+
+        if not url_or_id.startswith(("http://", "https://")):
+            url_or_id = "https://" + url_or_id
+
+        result = urlparse(url_or_id)
+
+        params = parse_qs(result.query)
+        if "v" in params and params["v"]:
+            return params["v"][0]
+
+        path = result.path.strip("/")
+
+        # Shorts, embed, and live
+        if any(x in path for x in ["shorts/", "embed/", "live/"]):
+            return path.split("/")[-1]
+
+        # Shortened link
+        if "youtu.be" in result.hostname:
+            return path.split("/")[0]
+
+        raise NoVideoFound("Couldn't extract video id.")
+
+    def fetch_metadata(self, url_or_id: str) -> VideoMetadata:
         """
         Returns a VideoMetadata instance containing:
             - `video_id`: YouTube video ID
@@ -31,7 +58,7 @@ class YouTubeAPI:
             - `short_description`: Short video description
 
         Args:
-            url (str): The URL of the YouTube video.
+            url_or_id (str): The URL or ID of the YouTube video.
 
         Returns:
             VideoMetadata: Video metadata object
@@ -40,19 +67,27 @@ class YouTubeAPI:
             NoVideoFound: No Video Found
             NoMetadataFound: No Metadata Found
         """
+        url: str = url_or_id
+        if not any(x in url_or_id for x in ["://", "youtube.com", "youtu.be"]):
+            url = f"https://youtu.be/{url_or_id}"
+
+        if not url.startswith(("http://", "https://")):
+            url = "https://" + url
+
         response = requests.get(url, headers=self._user_agent, timeout=10)
         if response.status_code != 200:
             raise NoVideoFound
 
         youtube_html = response.text
         soup = BeautifulSoup(youtube_html, "html.parser")
+
+        video_id: str = self._extract_video_id(url_or_id)
         try:
-            video_id = soup.find(name="meta", property="og:url").get("content")[32:]
-            title = soup.find(name="meta", property="og:title").get("content")
-            img_url = soup.find(name="meta", property="og:image").get("content")
-            short_description = soup.find(name="meta", property="og:description").get(
-                "content"
-            )
+            title: str = soup.find(name="meta", property="og:title").get("content")
+            img_url: str = soup.find(name="meta", property="og:image").get("content")
+            short_description: str = soup.find(
+                name="meta", property="og:description"
+            ).get("content")
         except Exception:
             raise NoMetadataFound
 
@@ -64,13 +99,13 @@ class YouTubeAPI:
         )
 
     def fetch_transcript(
-        self, url: str, language_code: str = "en", as_dict: bool = True
+        self, url_or_id: str, language_code: str = "en", as_dict: bool = True
     ) -> list[dict] | str:
         """
         Returns the transcript of the video in requested language.
 
         Args:
-            url (str): The URL of the YouTube video.
+            url_or_id (str): The URL or ID of the YouTube video.
             language_code (str, optional): The language code for the desired transcript. Defaults to "en".
             as_dict (bool, optional): If `True`, returns the transcript as a list of dictionaries;
                 otherwise, returns the transcript as a string. Defaults to `True`.
@@ -84,8 +119,7 @@ class YouTubeAPI:
             NoTranscriptFound: No Transcript Found
         """
         try:
-            data: VideoMetadata = self.fetch_metadata(url)
-            video_id = data.video_id
+            video_id: str = self._extract_video_id(url_or_id)
 
             ytt_api = YouTubeTranscriptApi()
             transcript_list = ytt_api.list(video_id)
@@ -117,13 +151,13 @@ class YouTubeAPI:
         )
 
     def fetch_all(
-        self, url: str, language_code: str = "en", as_dict: bool = True
+        self, url_or_id: str, language_code: str = "en", as_dict: bool = True
     ) -> tuple[VideoMetadata | None, list[dict] | str | None]:
         """
         Returns both video metadata and transcript. If there is an error, that spot in the tuple will have `None` instead of a value.
 
         Args:
-            url (str): The URL of the YouTube video.
+            url_or_id (str): The URL or ID of the YouTube video.
             language_code (str, optional): The language code for the desired transcript. Defaults to "en".
             as_dict (bool, optional): If `True`, returns the transcript as a list of dictionaries; otherwise, returns the transcript as a string. Defaults to `True`.
 
@@ -133,9 +167,9 @@ class YouTubeAPI:
                 - transcript (list[dict] | str | None): Video transcript, `None` if not found
         """
         try:
-            data = self.fetch_metadata(url)
+            data = self.fetch_metadata(url_or_id)
             transcript = self.fetch_transcript(
-                url=url, language_code=language_code, as_dict=as_dict
+                url_or_id=url_or_id, language_code=language_code, as_dict=as_dict
             )
         except (TranscriptsDisabled, NoTranscriptFound) as e:
             transcript = None
